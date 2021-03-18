0681f9862a2483ab1b2d411c3e8da2fb5d869796
DulebaKrzysztof <57409275+DulebaKrzysztof@users.noreply.github.com>
2021-03-18 15:12:41 +0100

    Create baza mój pomysł.sql

---
+++ Etap_3/baza mój pomysł.sql
@@ -0,0 +1,58 @@
+CREATE TABLE user(
+    id INT NOT NULL PRIMARY KEY,
+    login VARCHAR(50),
+    password VARCHAR(50),
+    email VARCHAR(50),
+    firstName VARCHAR(50),
+    lastName VARCHAR(50)
+);
+
+CREATE TABLE movie(
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    director VARCHAR(50) NOT NULL,
+    description VARCHAR(500),
+    duration VARCHAR(50)
+);
+
+CREATE TABLE auditorium(
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    seats_number INT NOT NULL
+);
+
+CREATE TABLE seat(
+    id INT NOT NULL PRIMARY KEY,
+    row INT NOT NULL,
+    num INT NOT NULL,
+    auditorium_id INT NOT NULL,
+    CONSTRAINT fk_auditorium_seat_id FOREIGN KEY (auditorium_id) REFERENCES auditorium(id)
+);
+
+CREATE TABLE screening(
+    id INT NOT NULL PRIMARY KEY,
+    screening_date DATE NOT NULL,
+    movie_id INT NOT NULL,
+    auditorium_id INT NOT NULL,
+    CONSTRAINT fk_auditorium_screening_id FOREIGN KEY (auditorium_id) REFERENCES auditorium(id),
+    CONSTRAINT fk_movie_screening_id FOREIGN KEY (movie_id) REFERENCES movie(id)
+);
+
+CREATE TABLE ticket(
+    id INT NOT NULL PRIMARY KEY,
+    ticket_type VARCHAR(50),
+    price VARCHAR(50)
+);
+
+CREATE TABLE reservation(
+    id INT(10) NOT NULL PRIMARY KEY,
+    movie_id INT NOT NULL,
+    auditorium_id INT NOT NULL,
+    ticket_id INT NOT NULL,
+    is_paid boolean NOT NULL,
+    user_id INT,
+    CONSTRAINT fk_movie_reservation_id FOREIGN KEY (movie_id) REFERENCES movie(id),
+    CONSTRAINT fk_auditorium_reservation_id FOREIGN KEY (auditorium_id) REFERENCES auditorium(id),
+    CONSTRAINT fk_ticket_reservation_id FOREIGN KEY (ticket_id) REFERENCES ticket(id),
+    CONSTRAINT fk_user_reservation_id FOREIGN KEY (user_id) REFERENCES user(id)
+);
