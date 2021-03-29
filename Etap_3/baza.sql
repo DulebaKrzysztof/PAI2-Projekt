8f1d030e52699c8862104b86ddf8ce8cbe4abb26
DulebaKrzysztof <57409275+DulebaKrzysztof@users.noreply.github.com>
2021-03-29 21:06:13 +0200

    Update baza.sql
    wyrzuciłem user_type,
    dodałem movie_actor, actor, genre, customer, employee, director

--- Etap_3/baza.sql
+++ Etap_3/baza.sql
@@ -8,109 +8,125 @@ DROP TABLE ticket_type;
 DROP TABLE screening;
 DROP TABLE seat;
 DROP TABLE auditorium;
+DROP TABLE movie_actor;
 DROP TABLE movie;
+DROP TABLE genre;
+DROP TABLE director;
+DROP TABLE actor;
 DROP TABLE snacks;
 DROP TABLE snacks_type;
 DROP TABLE invoice;
 DROP TABLE online_reservation;
+DROP TABLE customer;
+DROP TABLE employee;
 DROP TABLE user;
-DROP TABLE user_type;
-
-CREATE TABLE user_type(
-    id INT NOT NULL,
-    user_type VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE INDEX id_unique (id ASC)
-);
 
 CREATE TABLE user(
-    id INT NOT NULL,
-    login VARCHAR(50) NOT NULL,
+	id INT NOT NULL PRIMARY KEY,
+    login VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    firstName VARCHAR(50) NOT NULL,
-    lastName VARCHAR(50) NOT NULL,
-    user_type_id INT NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE INDEX id_unique (id ASC),
-    UNIQUE INDEX login_unique (login ASC),
-    UNIQUE INDEX email_unique (email ASC),
-    CONSTRAINT fk_user_type_user FOREIGN KEY (user_type_id) REFERENCES user_type(id)
+    email VARCHAR(50) NOT NULL UNIQUE,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE customer(
+	id INT NOT NULL PRIMARY KEY,
+	user_id INT NOT NULL UNIQUE,
+    CONSTRAINT fk_user_customer FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+CREATE TABLE employee(
+	id INT NOT NULL PRIMARY KEY,
+    user_id INT NOT NULL UNIQUE,
+    CONSTRAINT fk_user_employee FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+CREATE TABLE genre(
+	id INT NOT NULL PRIMARY KEY,
+	genre VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE director(
+	id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE actor(
+	id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE movie(
-    id INT NOT NULL,
+    id INT NOT NULL PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
-    director VARCHAR(50) NOT NULL,
+    genre_id INT NOT NULL,
+    director_id INT NOT NULL,
     description VARCHAR(255),
     duration VARCHAR(50),
-    PRIMARY KEY (id),
-    UNIQUE INDEX id_unique (id ASC)
+	CONSTRAINT fk_genre_movie FOREIGN KEY (genre_id) REFERENCES genre(id),
+    CONSTRAINT fk_director_movie FOREIGN KEY (director_id) REFERENCES director(id)
+);
+
+CREATE TABLE movie_actor(
+	movie_id INT NOT NULL,
+    actor_id INT NOT NULL,
+    PRIMARY KEY (movie_id, actor_id),
+    CONSTRAINT fk_movie_actor_movie FOREIGN KEY (movie_id) REFERENCES movie(id),
+    CONSTRAINT fk_movie_actor_actor FOREIGN KEY (actor_id) REFERENCES actor(id)
 );
 
 CREATE TABLE auditorium(
-    id INT NOT NULL,
+    id INT NOT NULL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    seats_number INT NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE INDEX id_unique (id ASC)
+    seats_number INT NOT NULL
 );
 
 CREATE TABLE seat(
-    id INT NOT NULL,
+    id INT NOT NULL PRIMARY KEY,
     seat_row INT NOT NULL,
     seat_num INT NOT NULL,
     auditorium_id INT NOT NULL,
-    PRIMARY KEY (id),
-    CONSTRAINT fk_auditorium_seat FOREIGN KEY (auditorium_id) REFERENCES auditorium(id),
-    UNIQUE INDEX id_unique (id ASC)
+    CONSTRAINT fk_auditorium_seat FOREIGN KEY (auditorium_id) REFERENCES auditorium(id)
 );
 
 CREATE TABLE screening(
-    id INT NOT NULL,
+    id INT NOT NULL PRIMARY KEY,
     screening_date DATE NOT NULL,
     movie_id INT NOT NULL,
     auditorium_id INT NOT NULL,
-    PRIMARY KEY (id),
     CONSTRAINT fk_auditorium_screening FOREIGN KEY (auditorium_id) REFERENCES auditorium(id),
-    CONSTRAINT fk_movie_screening FOREIGN KEY (movie_id) REFERENCES movie(id),
-    UNIQUE INDEX id_unique (id ASC)
+    CONSTRAINT fk_movie_screening FOREIGN KEY (movie_id) REFERENCES movie(id)
 );
 
 CREATE TABLE ticket_type(
-    id INT NOT NULL,
+    id INT NOT NULL PRIMARY KEY,
     ticket_type VARCHAR(50),
-    price VARCHAR(50),
-    PRIMARY KEY (id),
-    UNIQUE INDEX id_unique (id ASC)
+    price VARCHAR(50)
 );
 
 CREATE TABLE invoice(
-    id INT NOT NULL,
-    invoice_date DATE NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE INDEX id_unique (id ASC)
+    id INT NOT NULL PRIMARY KEY,
+    invoice_date DATE NOT NULL
 );
 
 CREATE TABLE online_reservation(
-    id INT NOT NULL,
-    user_id INT NOT NULL,
-    is_paid BOOLEAN NOT NULL DEFAULT 0,
+    id INT NOT NULL PRIMARY KEY,
+    customer_id INT NOT NULL,
+	is_paid BOOLEAN NOT NULL DEFAULT 0,
     is_canceled BOOLEAN NOT NULL DEFAULT 0,
-    PRIMARY KEY (id),
-    UNIQUE INDEX id_unique (id ASC),
-    CONSTRAINT fk_user_reservation FOREIGN KEY (user_id) REFERENCES user(id)
+    CONSTRAINT fk_customer_online_reservation FOREIGN KEY (customer_id) REFERENCES customer(id)
 );
 
 CREATE TABLE ticket(
-    id INT NOT NULL,
+    id INT NOT NULL PRIMARY KEY,
     screening_id INT NOT NULL,
     seat_id INT NOT NULL,
     ticket_type_id INT NOT NULL,
     invoice_id INT NOT NULL,
     online_reservation_id INT,
-    PRIMARY KEY (id),
-    UNIQUE INDEX id_unique (id ASC),
     CONSTRAINT fk_screening_ticket FOREIGN KEY (screening_id) REFERENCES screening(id),
     CONSTRAINT fk_seat_ticket FOREIGN KEY (seat_id) REFERENCES seat(id),
     CONSTRAINT fk_ticket_type_ticket FOREIGN KEY (ticket_type_id) REFERENCES ticket_type(id),
@@ -119,19 +135,16 @@ CREATE TABLE ticket(
 );
 
 CREATE TABLE snacks_type(
-    id INT NOT NULL,
+    id INT NOT NULL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    price INT NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE INDEX id_unique (id ASC)
+    price INT NOT NULL
 );
 
 CREATE TABLE snacks(
-    id INT NOT NULL,
-    snacks_type_id INT NOT NULL,
+    id INT NOT NULL PRIMARY KEY,
+    number INT NOT NULL,
+	snacks_type_id INT NOT NULL,
     invoice_id INT NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE INDEX id_unique (id ASC),
     CONSTRAINT fk_snacks_type_snacks FOREIGN KEY (snacks_type_id) REFERENCES snacks_type(id),
     CONSTRAINT fk_invoice_snacks FOREIGN KEY (invoice_id) REFERENCES invoice(id)
 );
